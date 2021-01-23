aed8b4d7ee863381c95bcfcd3c6ead062236c5d6
DannyR94 <russelldan994@gmail.com>
2021-01-22 23:20:01 -0500

    Practiced coding in the car ride to ohio, new file

---
+++ carride/main.js
@@ -0,0 +1,6 @@
+// Some hippie dippie shit here
+const eqzr = 100;
+
+function star(firstName, lastName, birthYear, birthMonth){
+    console.log((firstName.length + lastName.length * birthYear / birthMonth.length) / eqzr);
+}
