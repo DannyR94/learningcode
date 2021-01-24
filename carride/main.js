50940e1ca1aa8ac32469b8d2975858d0ffaa658d
DannyR94 <russelldan994@gmail.com>
2021-01-24 01:43:51 -0500

    Did some work

--- carride/main.js
+++ carride/main.js
@@ -4,3 +4,19 @@ const eqzr = 100;
 function star(firstName, lastName, birthYear, birthMonth){
     console.log((firstName.length + lastName.length * birthYear / birthMonth.length) / eqzr);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create string/array "id-key" from all the values returned by the questionire and call on that for each part of the "horoscope"
