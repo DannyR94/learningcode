aa3298c6047b2f08c62cc0f165c5580e9b63cb21
DannyR94 <russelldan994@gmail.com>
2021-01-24 19:05:35 -0500

    Added navbar to all pages

--- project_s/main.js
+++ project_s/main.js
@@ -1,14 +1,3 @@
-var myNavBar = document.querySelector('#navbar li');
-
-myNavBar.addEventListener('mouseover', navHoverOn, false);
-myNavBar.addEventListener('mouseout', navHoverOff, false);
-
-function navHoverOn()
-{
-    myNavBar.setAttribute('style', 'background-color:green;',)
-}
-
-function navHoverOff()
-{
-    myNavBar.setAttribute('style', 'background-color:grey;')
+function easter() {
+    window.alert("Your already on this page silly!");
 }
