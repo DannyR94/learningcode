7b6be10d1ad2ce5d1aad372f8664e07929d93747
DannyR94 <russelldan994@gmail.com>
2021-02-01 14:16:24 -0500

    added section3 to JSnotes

--- carride/main.js
+++ carride/main.js
@@ -1,39 +1,20 @@
-var details = [
-    'd', 'a', 'n', 'i', 'e', 'l'
- ];
- var isPresent;
- var records = [];
- var matchWords = ['a', 'e'];
- for (var index = 0; index < details.length; index++){
-    isPresent = true;
-    for (var outer = 0; outer< matchWords.length; outer++) {
-       if (details[index].indexOf(matchWords[outer]) === -1) {
-          isPresent = false;
-          break;
-       }
-    }
-    if (isPresent){
-       records.push(details[index]);
-    }
- }
- console.log(records)
+var students = [
+   {
+      name: 'Coolio',
+      age: 20,
+      grade: 'A+'
+   },
+   {
+      name: 'Mark',
+      age: 45,
+      grade: 'C+'  
+   }
+]
 
+students.forEach(function(student){
+   console.log(student);
+});
 
-
-
-
-
-
-
-// var my_array = ['d', 'a', 'n', 'i' ,'e' ,'l']
-
-
-// var find = my_array.filter(function(result) {
-//     return result.param1 === 'a' || result.param2 === 'e';
-// });
-
-// console.log(find);
-
-
-
-// create string/array "id-key" from all the values returned by the questionire and call on that for each part of the "horoscope"
+// for (var i = 0; i < students.length; i++) {
+//    console.log(students[i]);
+// }
