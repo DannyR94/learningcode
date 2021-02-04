b043f579579d34c3bae41f9c6ccb4b7a71204d91
DannyR94 <russelldan994@gmail.com>
2021-02-04 14:05:16 -0500

    more section3

--- JSnotes/section3/dom.js
+++ JSnotes/section3/dom.js
@@ -127,18 +127,38 @@ var itemList = document.querySelector('#items');
 // 
 
 // previousSibling
-console.log(itemList.previousSibling);
+// console.log(itemList.previousSibling);
 // previousElementSibling
-console.log(itemList.previousElementSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green'
 
+// createElement
 
+// Create a div
+var newDiv = document.createElement('div');
 
+// Add class
+newDiv.className = 'hello';
 
+// Add ID
+newDiv.id = 'hello1';
 
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
 
+// Create text node
+var newDivText = document.createTextNode('Hello World');
 
+// Add text to div
+newDiv.appendChild(newDivText);
 
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
 
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
 
 
 
