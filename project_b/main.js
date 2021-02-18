76d683c4e41607b7d1787fb640e6780d4b6a7f7b
DannyR94 <russelldan994@gmail.com>
2021-02-18 05:18:50 -0500

    Added exponential pricing

--- project_b/main.js
+++ project_b/main.js
@@ -9,7 +9,7 @@ var li = document.createElement('li');
 var input = document.createElement('input');
 
 // Start the game
-var bank = 0;
+var bank = 100000;
 moneyBal.innerHTML = '$'+ bank;
 
 // Manual Clicking
@@ -17,6 +17,7 @@ var clkWorth = 1;
 clickBtn.addEventListener('click', basicClk);
 function basicClk() {
     clkWorth = 1 + ((cfeStndAmt * 1) + (cfeBrwyAmt * 5) + (cfeSiloAmt * 15) + (cfeFctyAmt * 30) + (cfePortAmt * 100) + (cfeXAmt * 500) + (cfeUnvsAmt * 1000));
+    console.log(1 + ((cfeStndAmt * 1) + (cfeBrwyAmt * 5) + (cfeSiloAmt * 15) + (cfeFctyAmt * 30) + (cfePortAmt * 100) + (cfeXAmt * 500) + (cfeUnvsAmt * 1000)));
     bank += clkWorth;
     moneyBal.innerHTML = '$'+ bank;
 }
@@ -29,173 +30,185 @@ setInterval(function(){
 
 // Coffee Stand Elements
 var hasCfeStnd = false;
+var cfeStndBase = 50;
 var cfeStndAmt = 0;
 const cfeStnd = document.getElementById('cfeStnd');
 const cfeStndCntr = document.getElementById('cfeStndCntr');
 // Buys a Coffee Stand
 cfeStnd.addEventListener('click', buyCfeStnd);
 function buyCfeStnd() {
-    if(hasCfeStnd === false && bank >= 50){
+    if(hasCfeStnd === false && bank >= cfeStndBase){
         cfeBrwy.style.display = 'inherit';
-        bank -= 50;
+        bank -= cfeStndBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfeStnd = true;
         cfeStndAmt += 1;
         cfeStndCntr.textContent = "x"+cfeStndAmt;
-    } else if(hasCfeStnd === true && bank >= 50){
-        bank -= 50;
+    } else if(hasCfeStnd === true && bank >= Math.ceil(cfeStndBase * Math.pow(1.15, cfeStndAmt))){
+        bank -= Math.ceil(cfeStndBase * Math.pow(1.15, cfeStndAmt));
+        // console.log("You Payed "+ Math.ceil(cfeStndBase * Math.pow(1.15, cfeStndAmt)));
         moneyBal.innerHTML = '$'+ bank;
         cfeStndAmt += 1;
         cfeStndCntr.textContent = "x"+cfeStndAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfeStndBase * Math.pow(1.15, cfeStndAmt))+' to buy that!');
     }
 }
 
 // Coffee Brewery Elements
 var hasCfeBrwy = false;
+var cfeBrwyBase = 500;
 var cfeBrwyAmt = 0;
 const cfeBrwy = document.getElementById('cfeBrwy');
 const cfeBrwyCntr = document.getElementById('cfeBrwyCntr');
 cfeBrwy.addEventListener('click', buyCfeBrwy);
 // Buys a Coffee Brewery
 function buyCfeBrwy() {
-    if(hasCfeBrwy === false && bank >= 500){
+    if(hasCfeBrwy === false && bank >= cfeBrwyBase){
         cfeSilo.style.display = 'inherit';
-        bank -= 500;
+        bank -= cfeBrwyBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfeBrwy = true;
         cfeBrwyAmt += 1;
         cfeBrwyCntr.textContent = "x"+cfeBrwyAmt;
-    } else if(hasCfeBrwy === true && bank >= 500){
-        bank -= 500;
+    } else if(hasCfeBrwy === true && bank >= Math.ceil(cfeBrwyBase * Math.pow(1.15, cfeBrwyAmt))){
+        bank -= Math.ceil(cfeBrwyBase * Math.pow(1.15, cfeBrwyAmt));
+        // console.log("You Payed "+ Math.ceil(cfeBrwyBase * Math.pow(1.15, cfeBrwyAmt)));
         moneyBal.innerHTML = '$'+ bank;
         cfeBrwyAmt += 1;
         cfeBrwyCntr.textContent = "x"+cfeBrwyAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfeBrwyBase * Math.pow(1.15, cfeBrwyAmt))+' to buy that!');
     }
 }
 
 // Coffee Silo Elements
 var hasCfeSilo = false;
+var cfeSiloBase = 5000;
 var cfeSiloAmt = 0;
 const cfeSilo = document.getElementById('cfeSilo');
 const cfeSiloCntr = document.getElementById('cfeSiloCntr');
 cfeSilo.addEventListener('click', buyCfeSilo);
 // Buys a Coffee Silo
 function buyCfeSilo() {
-    if(hasCfeSilo === false && bank >= 5000){
+    if(hasCfeSilo === false && bank >= cfeSiloBase){
         cfeFcty.style.display = 'inherit';
-        bank -= 5000;
+        bank -= cfeSiloBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfeSilo = true;
         cfeSiloAmt += 1;
         cfeSiloCntr.textContent = "x"+cfeSiloAmt;
-    } else if(hasCfeSilo === true && bank >= 5000){
-        bank -= 5000;
+    } else if(hasCfeSilo === true && bank >= Math.ceil(cfeSiloBase * Math.pow(1.15, cfeSiloAmt))){
+        bank -= Math.ceil(cfeSiloBase * Math.pow(1.15, cfeSiloAmt));
         moneyBal.innerHTML = '$'+ bank;
         cfeSiloAmt += 1;
         cfeSiloCntr.textContent = "x"+cfeSiloAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfeSiloBase * Math.pow(1.15, cfeSiloAmt))+' to buy that!');
     }
 }
 
 // Coffee Factory Elements
 var hasCfeFcty = false;
+var cfeCfeBase = 50000;
 var cfeFctyAmt = 0;
 const cfeFcty = document.getElementById('cfeFcty');
 const cfeFctyCntr = document.getElementById('cfeFctyCntr');
 cfeFcty.addEventListener('click', buyCfeFcty);
 // Buys a Coffee Factory
 function buyCfeFcty() {
-    if(hasCfeFcty === false && bank >= 50000){
+    if(hasCfeFcty === false && bank >= cfeCfeBase){
         cfePort.style.display = 'inherit';
-        bank -= 50000;
+        bank -= cfeCfeBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfeFcty = true;
         cfeFctyAmt += 1;
         cfeFctyCntr.textContent = "x"+cfeFctyAmt;
-    } else if(hasCfeFcty === true && bank >= 50000){
-        bank -= 50000;
+    } else if(hasCfeFcty === true && bank >= Math.ceil(cfeCfeBase * Math.pow(1.15, cfeFctyAmt))){
+        bank -= Math.ceil(cfeCfeBase * Math.pow(1.15, cfeFctyAmt));
         moneyBal.innerHTML = '$'+ bank;
         cfeFctyAmt += 1;
         cfeFctyCntr.textContent = "x"+cfeFctyAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfeCfeBase * Math.pow(1.15, cfeFctyAmt))+' to buy that!');
     }
 }
 
 // Coffee Port Elements
 var hasCfePort = false;
+var cfePortBase = 500000;
 var cfePortAmt = 0;
 const cfePort = document.getElementById('cfePort');
 const cfePortCntr = document.getElementById('cfePortCntr');
 cfePort.addEventListener('click', buyCfePort);
 // Buys a Coffee Port
 function buyCfePort() {
-    if(hasCfePort === false && bank >= 500000){
+    if(hasCfePort === false && bank >= cfePortBase){
         cfeX.style.display = 'inherit';
+        bank -= cfePortBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfePort = true;
         cfePortAmt += 1;
         cfePortCntr.textContent = "x"+cfePortAmt;
-    } else if(hasCfePort === true && bank >= 500000){
-        bank -= 500000;
+    } else if(hasCfePort === true && bank >= Math.ceil(cfePortBase * Math.pow(1.15, cfePortAmt))){
+        bank -= Math.ceil(cfePortBase * Math.pow(1.15, cfePortAmt));
         moneyBal.innerHTML = '$'+ bank;
         cfePortAmt += 1;
         cfePortCntr.textContent = "x"+cfePortAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfePortBase * Math.pow(1.15, cfePortAmt))+' to buy that!');
     }
 }
 
 // CoffeeX Elements
 var hasCfeX = false;
+var cfeXBase = 5000000;
 var cfeXAmt = 0;
 const cfeX = document.getElementById('cfeX');
 const cfeXCntr = document.getElementById('cfeXCntr');
 cfeX.addEventListener('click', buyCfeX);
 // Buys a CoffeeX
 function buyCfeX() {
-    if(hasCfeX === false && bank >= 5000000){
+    if(hasCfeX === false && bank >= cfeXBase){
         cfeUnvs.style.display = 'inherit';
+        bank -= cfeXBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfeX = true;
         cfeXAmt += 1;
         cfeXCntr.textContent = "x"+cfeXAmt;
-    } else if(hasCfeX === true && bank >= 5000000){
-        bank -= 5000000;
+    } else if(hasCfeX === true && bank >= Math.ceil(cfeXBase * Math.pow(1.15, cfeXAmt))){
+        bank -= Math.ceil(cfeXBase * Math.pow(1.15, cfeXAmt));
         moneyBal.innerHTML = '$'+ bank;
         cfeXAmt += 1;
         cfeXCntr.textContent = "x"+cfeXAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfeXBase * Math.pow(1.15, cfeXAmt))+' to buy that!');
     }
 }
 
 // Coffee Universe Elements
 var hasCfeUnvs = false;
+var cfeUnvsBase = 50000000;
 var cfeUnvsAmt = 0;
 const cfeUnvs = document.getElementById('cfeUnvs');
 const cfeUnvsCntr = document.getElementById('cfeUnvsCntr');
 cfeUnvs.addEventListener('click', buyCfeUnvs);
 // Buys a Coffee Universe
 function buyCfeUnvs() {
-    if(hasCfeUnvs === false && bank >= 50000000){
+    if(hasCfeUnvs === false && bank >= cfeUnvsBase){
         // cfeUnvs.style.display = 'inherit';
+        bank -= cfeUnvsBase;
         moneyBal.innerHTML = '$'+ bank;
         hasCfeUnvs = true;
         cfeUnvsAmt += 1;
         cfeUnvsCntr.textContent = "x"+cfeUnvsAmt;
-    } else if(hasCfeUnvs === true && bank >= 50000000){
-        bank -= 50000000;
+    } else if(hasCfeUnvs === true && bank >= Math.ceil(cfeUnvsBase * Math.pow(1.15, cfeUnvsAmt))){
+        bank -= Math.ceil(cfeUnvsBase * Math.pow(1.15, cfeUnvsAmt));
         moneyBal.innerHTML = '$'+ bank;
         cfeUnvsAmt += 1;
         cfeUnvsCntr.textContent = "x"+cfeUnvsAmt;
     } else {
-        alert('you need more money');
+        alert('You need $'+Math.ceil(cfeUnvsBase * Math.pow(1.15, cfeUnvsAmt))+' to buy that!');
     }
 }
 
